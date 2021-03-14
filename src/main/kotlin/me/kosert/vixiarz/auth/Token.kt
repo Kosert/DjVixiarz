b0ec28783cdf969df584ab3c58d09b35baa9d16e
kosert <kosert@kosert.me>
2021-03-14 19:31:19 +0100

    add secret.json support

--- src/main/kotlin/me/kosert/vixiarz/auth/Token.kt
+++ src/main/kotlin/me/kosert/vixiarz/auth/Token.kt
@@ -1,9 +1,27 @@
 package me.kosert.vixiarz.auth
 
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import java.io.File
+
+private const val SECRET_FILENAME = "secret.json"
+
+private class SecretJson(
+    @SerializedName("discordToken")
+    val token: String
+)
+
 object Token {
 
+    private var token: String = ""
+
     fun loadFromFile() {
-        //TODO
+        val json = File(SECRET_FILENAME).readText()
+        val gson = Gson()
+        token = gson.fromJson(json, SecretJson::class.java).token
+        println("Token loaded")
     }
+
+    fun get() = token
 
 }
--- src/main/kotlin/me/kosert/vixiarz/main.kt
+++ src/main/kotlin/me/kosert/vixiarz/main.kt
@@ -1,12 +1,50 @@
 package me.kosert.vixiarz
 
 import discord4j.core.DiscordClientBuilder
+import discord4j.core.GatewayDiscordClient
+import me.kosert.vixiarz.auth.Token
+import discord4j.core.`object`.entity.User
+
+import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.`object`.entity.Message
+import discord4j.core.`object`.entity.channel.MessageChannel
+
+import discord4j.core.event.domain.message.MessageCreateEvent
+import org.reactivestreams.Publisher
+import java.util.function.Function
+
 
 suspend fun main() {
-	println("Starting")
+    println("Starting")
+    Token.loadFromFile()
+    val client = DiscordClientBuilder.create(Token.get())
+            .build()
+            .login()
+            .block()!!
 
-	//val builder = DiscordClientBuilder.create("TOKEN HERE")
+    client.eventDispatcher.on(ReadyEvent::class.java)
+            .subscribe { event: ReadyEvent ->
+                val self = event.self
+                println("Logged in as ${self.username}#${self.discriminator}")
+            }
 
+    client.eventDispatcher.on(MessageCreateEvent::class.java)
+            .map { obj: MessageCreateEvent -> obj.message }
+            .filter {
+                message: Message -> message.author.map { user: User -> !user.isBot }.orElse(false)
+            }
+            .filter {
+                message: Message -> message.content.equals("!ping", ignoreCase = true)
+            }
+            .flatMap<MessageChannel>(Function<Message, Publisher<out MessageChannel>> {
+                obj: Message -> obj.channel
+            })
+            .flatMap(Function<MessageChannel, Publisher<out Message>> {
+                channel: MessageChannel -> channel.createMessage("Pong!")
+            })
+            .subscribe()
+
+    client.onDisconnect().block()
 
 
 }
